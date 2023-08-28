503e3f42c438fe90ec8041a7d07c5a3e4e7d78ab
Locretia <kw0498@gmx.de>
2023-08-28 12:38:19 +0200

    Flowmap Control Window added

---
+++ Godot/Flowmap/FlowmapControl.gd
@@ -0,0 +1,28 @@
+extends WindowDialog
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_FlowmapControlButton_pressed():
+	self.show()
+
+
+func _on_IterateForwardCheck_pressed():
+	pass # Replace with function body.
+
+
+func _on_IterateBackwardCheck_pressed():
+	pass # Replace with function body.
